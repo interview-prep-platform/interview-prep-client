609a285fd0c3cbf9c428d9c6c995b9520d7ac203
zcr003 <zramsey1@cnm.edu>
2021-12-16 10:56:35 -0700

    Updates to question Fragment and Layout.

--- app/src/main/java/edu/cnm/deepdive/interviewprep/controller/QuizPageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/interviewprep/controller/QuizPageFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.interviewprep.R;
 import edu.cnm.deepdive.interviewprep.databinding.FragmentQuizPageBinding;
+import edu.cnm.deepdive.interviewprep.model.Question;
 import edu.cnm.deepdive.interviewprep.viewmodel.QuestionViewModel;
 import java.util.UUID;
 
@@ -49,6 +54,33 @@ public class QuizPageFragment extends Fragment {
           binding.answerText.setText(question.getAnswer());
           binding.sourceText.setText(question.getSource());
         });
+    binding.answerText.setVisibility(View.GONE);
+    binding.showAnswer.setText(R.string.show_answer_button);
+    binding.showAnswer.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (binding.showAnswer.getText().toString().equals("Show Answer")) {
+          binding.answerText.setVisibility(View.VISIBLE);
+          binding.showAnswer.setText(R.string.hide_answer_button);
+        } else {
+          binding.showAnswer.setText(R.string.show_answer_button);
+          binding.answerText.setVisibility(View.GONE);
+        }
+      }
+    });
+    binding.submit.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        binding.userAnswerText.onEditorAction(EditorInfo.IME_ACTION_DONE);
+        questionViewModel.getQuestion().observe(getViewLifecycleOwner(), (q) -> {
+          q.setUserAnswer(binding.userAnswerText.getText().toString().trim());
+          questionViewModel//can only observe on live data
+              .updateQuestion(q);
+        });
+        Toast.makeText(
+            getContext(), "Your Answer has been Submitted", Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 
   @Override
